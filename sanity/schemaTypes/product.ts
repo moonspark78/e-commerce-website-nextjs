a9b25a368ce34b6af316bb59a3bf77b313a0cf26
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-04-24 15:56:04 +0200

    :tada: creating the type   :tada:

---
+++ sanity/schemaTypes/product.ts
@@ -0,0 +1,24 @@
+import { title } from "process";
+
+export default {
+    name: "product",
+    type: "document",
+    title: "Product",
+    filds: [
+        {
+            name: "name",
+            type: "string",
+            title: "Name of Product",
+        },
+        {
+            name: "name",
+            type: "string",
+            title: "Name of Product",
+        },
+        {
+            name: "name",
+            type: "string", 
+            title: "Name of Product",
+        },
+    ]
+}
