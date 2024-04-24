b06ebef06cfcd2302f94908e412b4b68e19cf9cb
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-04-24 16:33:39 +0200

    :tada: done with Products Type   :tada:

--- sanity/schemaTypes/product.ts
+++ sanity/schemaTypes/product.ts
@@ -4,21 +4,35 @@ export default {
     name: "product",
     type: "document",
     title: "Product",
-    filds: [
+    fields: [
         {
             name: "name",
             type: "string",
             title: "Name of Product",
         },
         {
-            name: "name",
-            type: "string",
-            title: "Name of Product",
+            name: "image",
+            type: "array",
+            title: "Products Images",
+            of: [{type: "image"}],
         },
         {
-            name: "name",
-            type: "string", 
-            title: "Name of Product",
+            name: "description",
+            type: "text", 
+            title: "Description of Product",
+        },
+        {
+            name: "slug",
+            type: "slug", 
+            title: "Product Slug",
+            options:{
+                source: "name"
+            },
+        },
+        {
+            name: "price",
+            type: "number", 
+            title: "Price",
         },
     ]
 }
