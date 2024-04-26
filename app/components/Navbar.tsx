fb378f6e44bc7ca69ffc8af30d090cc97401822e
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-04-26 13:59:48 +0200

    :tada: working on the navbar  :tada:

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,27 @@
+import { headers } from 'next/headers'
+import Link from 'next/link'
 import React from 'react'
+
+
+const links = [
+  { name: "Home", href: "/"},
+  { name: "Men", href: "/Men"},
+  { name: "Women", href: "/Women"},
+  { name: "Teens", href: "/Teens"},
+]
 
 const Navbar = () => {
   return (
-    <div>
-        <h1>Hello nav</h1>
-    </div>
+    <header className='mb-8 border-b'>
+      <div className='flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl'>
+          <Link href="/">
+              <h1 className='text-4xl font-bold'>Next <span className='text-primary'>Commerce</span> </h1>
+          </Link>
+
+
+          
+      </div>
+    </header>
   )
 }
 
